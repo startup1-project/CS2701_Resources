52be25d32fe700ece5e243f2b06f9ea0f3880025
NABBrunel <Nadine.Aburumman@brunel.ac.uk>
2025-11-16 13:39:50 +0000

    Delete Labs/Week 8 Lab-React-App directory

--- Labs/Week 8 Lab-React-App/src/components/Sidebar.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Sidebar(){
-    return (
-        <aside className="sidebar">
-          <input type="text" className="search" id="search" name="search" placeholder="Search"></input>
-        </aside>
-    )
-}
